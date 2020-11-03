9167862293c7738687b849122d28dfc8f8191cd3
Diêgo Ferreira <diegofrr.19@gmail.com>
2020-11-03 14:06:06 -0300

    att

--- POO Project/Project/Interfaces/InterfaceMenus.java
+++ POO Project/Project/Interfaces/InterfaceMenus.java
@@ -8,5 +8,6 @@ public interface InterfaceMenus {
 	public String menuAcessarCurso(Curso _cursoAcessado);
 	public String menuSecundario(Aluno _alunoLogado);
 	public String menuPrincipal();
+	public String menuEstatisticasProfessor();
 	
 }
